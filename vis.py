10552652c53f5c30915954283d006eaae9a0c8ef
Pedro Neto <pasoneto@gmail.com>
2023-12-11 11:16:32 +0200

    remove another 3d

--- vis.py
+++ vis.py
@@ -10,8 +10,8 @@ import soundfile as sf
 import torch
 from matplotlib import cm
 from matplotlib.colors import ListedColormap
-from pytorch3d.transforms import (axis_angle_to_quaternion, quaternion_apply,
-                                  quaternion_multiply)
+#from pytorch3d.transforms import (axis_angle_to_quaternion, quaternion_apply,
+#                                  quaternion_multiply)
 from tqdm import tqdm
 
 smpl_joints = [
